4f61d3451312a41f790e5dc173cc40fbe4ca231a
Sergei Trubchik <sergeitrubchik95@gmail.com>
2014-11-04 11:58:42 +0300

    FirstCommit

---
+++ matrix/src/matrix/m1.java
@@ -0,0 +1,20 @@
+package matrix;
+
+
+
+public class m1 {
+
+	public static void main(String[] args) {
+		/*int[] myArray= new int[10];
+		int i ;
+		for(i=0;i<10;i++){
+			myArray[i]=i;
+		}
+		for(i=0; i<10; i++){
+			System.out.println("a["+i+"] = "+myArray[i]);
+		}*/
+		System.out.println("Hello, Sergei!)");
+
+	}
+
+}
